4eec7edd6319d8f79b5071726f3942f809f01848
Tomáš Mesároš <tomas.mesaros.22@gmail.com>
2025-06-05 19:59:07 +0200

    vytvorenie footer: obsah, style

---
+++ src/lib/component/Footer.svelte
@@ -0,0 +1,18 @@
+<footer class="footer">
+    <p>&copy; 2025 Hej-Voso. Všetky práva vyhradené.</p>
+</footer>
+
+<style>
+    .footer{
+        width: 100%;
+        background-color: var(--primary-color);
+
+    }
+    
+    p{
+        text-align: center;
+        padding: 0.5rem;
+        font-size: var(--font-size-sm);
+        color: var(--text-color);
+    }
+</style>
