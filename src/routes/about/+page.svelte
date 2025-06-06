dde9738f2005cd4bfd853947ab2231a370d36fbb
Tomáš Mesároš <tomas.mesaros.22@gmail.com>
2025-06-06 21:31:52 +0200

    vytvorenie about: sekcii, style

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+
+</script>
+
+<section class="flex-column">
+    <h2>O mne - Patrik Rychnák</h2>
+
+    <div class="section flex-column">
+        <h3>Natpis sekcie</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatibus, tenetur laborum quos excepturi quas illo officia rerum deserunt debitis voluptatum ipsa quibusdam sint nisi distinctio earum, quae unde aliquam!
+        Laborum et tempore architecto, iste aspernatur perferendis quisquam est distinctio aliquid fuga, obcaecati atque quas? Minus nesciunt vel impedit. Illo nam ab culpa, facere vitae nobis necessitatibus autem expedita! Nesciunt.
+        Cupiditate aut sapiente pariatur facere numquam necessitatibus odio cum, facilis officiis fugit sequi deleniti alias debitis iusto suscipit accusamus aliquam ea illo mollitia quo nulla reiciendis quam. Doloribus, illum asperiores.
+        Saepe architecto exercitationem illo libero ut quibusdam facere in, officiis, sequi minus nobis ipsa autem voluptatum quasi quos dolorum reiciendis ex molestiae possimus ipsum esse doloremque impedit vitae tempora! Repellendus?
+        Hic minus alias molestias similique, rem sint. Temporibus maiores officia animi repellendus architecto, voluptates cumque similique mollitia blanditiis quam totam asperiores ullam tempora delectus dignissimos sunt cum odio eaque iusto.</p>
+    </div>
+        <div class="section flex-column">
+        <h3>Natpis sekcie</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatibus, tenetur laborum quos excepturi quas illo officia rerum deserunt debitis voluptatum ipsa quibusdam sint nisi distinctio earum, quae unde aliquam!
+        Laborum et tempore architecto, iste aspernatur perferendis quisquam est distinctio aliquid fuga, obcaecati atque quas? Minus nesciunt vel impedit. Illo nam ab culpa, facere vitae nobis necessitatibus autem expedita! Nesciunt.
+        Cupiditate aut sapiente pariatur facere numquam necessitatibus odio cum, facilis officiis fugit sequi deleniti alias debitis iusto suscipit accusamus aliquam ea illo mollitia quo nulla reiciendis quam. Doloribus, illum asperiores.
+        Saepe architecto exercitationem illo libero ut quibusdam facere in, officiis, sequi minus nobis ipsa autem voluptatum quasi quos dolorum reiciendis ex molestiae possimus ipsum esse doloremque impedit vitae tempora! Repellendus?
+        Hic minus alias molestias similique, rem sint. Temporibus maiores officia animi repellendus architecto, voluptates cumque similique mollitia blanditiis quam totam asperiores ullam tempora delectus dignissimos sunt cum odio eaque iusto.</p>
+    </div>
+        <div class="section flex-column">
+        <h3>Natpis sekcie</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatibus, tenetur laborum quos excepturi quas illo officia rerum deserunt debitis voluptatum ipsa quibusdam sint nisi distinctio earum, quae unde aliquam!
+        Laborum et tempore architecto, iste aspernatur perferendis quisquam est distinctio aliquid fuga, obcaecati atque quas? Minus nesciunt vel impedit. Illo nam ab culpa, facere vitae nobis necessitatibus autem expedita! Nesciunt.
+        Cupiditate aut sapiente pariatur facere numquam necessitatibus odio cum, facilis officiis fugit sequi deleniti alias debitis iusto suscipit accusamus aliquam ea illo mollitia quo nulla reiciendis quam. Doloribus, illum asperiores.
+        Saepe architecto exercitationem illo libero ut quibusdam facere in, officiis, sequi minus nobis ipsa autem voluptatum quasi quos dolorum reiciendis ex molestiae possimus ipsum esse doloremque impedit vitae tempora! Repellendus?
+        Hic minus alias molestias similique, rem sint. Temporibus maiores officia animi repellendus architecto, voluptates cumque similique mollitia blanditiis quam totam asperiores ullam tempora delectus dignissimos sunt cum odio eaque iusto.</p>
+    </div>
+        <div class="section flex-column">
+        <h3>Natpis sekcie</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatibus, tenetur laborum quos excepturi quas illo officia rerum deserunt debitis voluptatum ipsa quibusdam sint nisi distinctio earum, quae unde aliquam!
+        Laborum et tempore architecto, iste aspernatur perferendis quisquam est distinctio aliquid fuga, obcaecati atque quas? Minus nesciunt vel impedit. Illo nam ab culpa, facere vitae nobis necessitatibus autem expedita! Nesciunt.
+        Cupiditate aut sapiente pariatur facere numquam necessitatibus odio cum, facilis officiis fugit sequi deleniti alias debitis iusto suscipit accusamus aliquam ea illo mollitia quo nulla reiciendis quam. Doloribus, illum asperiores.
+        Saepe architecto exercitationem illo libero ut quibusdam facere in, officiis, sequi minus nobis ipsa autem voluptatum quasi quos dolorum reiciendis ex molestiae possimus ipsum esse doloremque impedit vitae tempora! Repellendus?
+        Hic minus alias molestias similique, rem sint. Temporibus maiores officia animi repellendus architecto, voluptates cumque similique mollitia blanditiis quam totam asperiores ullam tempora delectus dignissimos sunt cum odio eaque iusto.</p>
+    </div>
+        <div class="section flex-column">
+        <h3>Natpis sekcie</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatibus, tenetur laborum quos excepturi quas illo officia rerum deserunt debitis voluptatum ipsa quibusdam sint nisi distinctio earum, quae unde aliquam!
+        Laborum et tempore architecto, iste aspernatur perferendis quisquam est distinctio aliquid fuga, obcaecati atque quas? Minus nesciunt vel impedit. Illo nam ab culpa, facere vitae nobis necessitatibus autem expedita! Nesciunt.
+        Cupiditate aut sapiente pariatur facere numquam necessitatibus odio cum, facilis officiis fugit sequi deleniti alias debitis iusto suscipit accusamus aliquam ea illo mollitia quo nulla reiciendis quam. Doloribus, illum asperiores.
+        Saepe architecto exercitationem illo libero ut quibusdam facere in, officiis, sequi minus nobis ipsa autem voluptatum quasi quos dolorum reiciendis ex molestiae possimus ipsum esse doloremque impedit vitae tempora! Repellendus?
+        Hic minus alias molestias similique, rem sint. Temporibus maiores officia animi repellendus architecto, voluptates cumque similique mollitia blanditiis quam totam asperiores ullam tempora delectus dignissimos sunt cum odio eaque iusto.</p>
+    </div>
+</section>
+
+<style>
+    .section{
+        gap: 5px;
+        margin-bottom: 30px;
+        h3{
+            font-size: var(--font-size-lg);
+            color: var(--button-hover-color);
+        }
+        p{
+            font-size: var(--font-size-sm);
+        }
+    }
+</style>
