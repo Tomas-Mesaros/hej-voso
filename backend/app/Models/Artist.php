7bd2b2a062200b5b5a7bad513a09620879f3e008
Tomáš Mesároš <tomas.mesaros.22@gmail.com>
2025-06-29 00:07:43 +0200

    feat: přidán model Artist, testovací route, úprava nastavení terminálu a databáze

---
+++ backend/app/Models/Artist.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    // Ak máš tabuľku artists a ID je 'id', nemusíš nič riešiť
+    // Ak máš iný názov tabuľky, napíš:
+    // protected $table = 'artists';
+
+    // Ak nemáš timestamps, tak:
+    // public $timestamps = false;
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Models\Artist;
+
+Route::get('/artists-test', function () {
+    $artists = Artist::all();
+    return response()->json($artists);
+});
